8707722d2a70c5f668127f47d965bbf3434c82e0
Ashique <ashique12009@gmail.com>
2021-02-24 21:39:14 +0600

    Base controller added

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 //Include config
 require('config.php');
 require('classes/Bootstrap.php');
+require('classes/Controller.php');
 
 $bootstrap = new Bootstrap($_GET);
 $controller = $bootstrap->createController();
+if ($controller) {
+  $controller->executeAction();
+}
